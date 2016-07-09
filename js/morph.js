e9f3714bf5f95676c22a35415fcd618d9bf96358
Quinn <qlmoffat@gmail.com>
2016-07-09 19:01:14 +1200

    Changeeeeeessssss

--- js/morph.js
+++
@@ -1,7 +0,0 @@
- $("#js-rotating").Morphext({
-                animation: "bounce",
-                speed:2000,
-                complete: function () {
-                    console.log("This is called after a phrase is animated in! Current phrase index: " + this.index);
-                }
-            });
