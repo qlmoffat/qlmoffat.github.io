3fb16544c9a369b208c6aa713debfa8fa0a2959a
Quinn <qlmoffat@gmail.com>
2021-11-27 21:40:33 +1300

    adding blog

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/quinn/Desktop/profile/qlmoffat.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/quinn/Desktop/profile/qlmoffat.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/quinn/Desktop/profile/qlmoffat.github.io/src/pages/index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/quinn/Desktop/profile/qlmoffat.github.io/src/pages/using-typescript.tsx")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/quinn/Desktop/profile/qlmoffat.github.io/src/templates/blog-post.js"))
+}
+
